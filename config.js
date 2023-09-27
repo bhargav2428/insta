6959e083b389cbb820629c1bdd332fea3db8930d
chindada bhargav yaswanth <83665022+bhargav2428@users.noreply.github.com>
2023-09-27 20:48:34 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+ 
+    MY_KEY : '1935e3a3damshae463748630ee8cp1868bdjsn58908a89197d',
+    URL : "https://instagram120.p.rapidapi.com/api/instagram/links",
+    STORYURL:  "",
+    
+    SECRET_KEY : '56789',
+    
+    KEY_2 : 'b10b1a9ad1msh77eaeeffe71b59dp150379jsnf23da69938ce'
+    
+   }
