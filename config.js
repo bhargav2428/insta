9ee964c2ec611ab37b2989707906938c70a15cfd
chindada bhargav yaswanth <83665022+bhargav2428@users.noreply.github.com>
2023-09-28 11:09:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ var config = {
  
     MY_KEY : '3131a2438cmshd53e3fdf25896b7p198d5djsndcf65cc324f3',
     URL : "https://instagram120.p.rapidapi.com/api/instagram/links",
-    STORYURL:  "",
-    
-    SECRET_KEY : '56789',
-    
-    KEY_2 : 'b10b1a9ad1msh77eaeeffe71b59dp150379jsnf23da69938ce'
+    STORYURL:  "https://instagram120.p.rapidapi.com/api/instagram/profile",
+        
     
    }
